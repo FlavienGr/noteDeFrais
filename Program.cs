9ef90f01b833f80758151d83e507847dfb75fdf6
FlavGr <zencles75@gmail.com>
2020-10-21 22:11:40 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace notedefrais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
