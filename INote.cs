fdd59448af5920f6251a8f8332dee4ad9401d026
FlavGr <zencles75@gmail.com>
2020-10-22 22:27:29 +0200

    note de frais progress

---
+++ INote.cs
@@ -0,0 +1,14 @@
+﻿namespace notedefrais
+{
+    public interface INote
+    {
+        ICommercial AgentCommercial { get; set; }
+        decimal MontantARembourser { get; set; }
+        bool Rembourse { get; set; }
+
+        decimal CalculMontantARembourser();
+        void SetGiveMoneyBack();
+        void SetMontantARembourser();
+        void ToString();
+    }
+}
